d0ae57a27390ff89a498846674bf109ceb046405
riyoneri <lionkaneza@gmail.com>
2023-06-16 10:23:25 +0200

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        main: "",
+        main: "Pro-Text-Regular",
         "main-light": "",
-        "main-bold": ""
+        "main-bold": "Pro-Text-Bold",
+        "main-semi-bold": "Pro-Text-Semibold"
       },
       backgroundImage: {
-        
+        "racial-equity": "url(/assets/images/promo__sw58lbodkmqq_large.jpg)"
       },
       colors: {
+        "link": "#06c",
+        "my-zinc": "#231F20",
         "main": "",
         "hover": "",
+        "my-gray": "#F5F5F7"
       }
     },
   },
